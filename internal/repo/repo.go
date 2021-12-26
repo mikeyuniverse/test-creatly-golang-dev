abd82b43c931296b301126bad193c2dbc14e668e
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-26 11:23:07 +0300

    repo.go: Create interfaces for Users and Files deps

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -2,14 +2,19 @@ package repo
 
 import (
 	"creatly-task/internal/config"
+	"creatly-task/internal/models"
 	"creatly-task/internal/mongodb"
 )
 
-type Users interface{}
+type Users interface {
+	CreateUser(*models.UserSignUpInput) error
+}
 
 type Tokens interface{}
 
-type Files interface{}
+type Files interface {
+	All() ([]models.FileOut, error)
+}
 
 type Repo struct {
 	Users  Users
@@ -17,6 +22,7 @@ type Repo struct {
 	Files  Files
 }
 
+// TODO Implementation of interfaces
 func New(db *mongodb.Mongo, config *config.Repo) *Repo {
 	return &Repo{
 		Users:  newUsersRepo(db, config.UsersCollection),
