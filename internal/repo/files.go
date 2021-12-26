b9ac89176a295c5a284138bcbdd2c79d6ca64464
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-26 16:42:15 +0300

    files.go: Implemented All() method

--- internal/repo/files.go
+++ internal/repo/files.go
@@ -1,8 +1,11 @@
 package repo
 
 import (
+	"context"
+	"creatly-task/internal/models"
 	"creatly-task/internal/mongodb"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -15,4 +18,20 @@ func newFilesRepo(mongo *mongodb.Mongo, collectionName string) *FilesRepo {
 	return &FilesRepo{
 		db: collection,
 	}
+}
+
+func (f *FilesRepo) All() ([]models.FileOut, error) {
+	cursor, err := f.db.Find(context.TODO(), bson.M{})
+	if err != nil {
+		return []models.FileOut{}, err
+	}
+
+	results := make([]models.FileOut, 1)
+	err = cursor.All(context.Background(), &results)
+	if err != nil {
+		return []models.FileOut{}, err
+	}
+
+	return results, nil
+
 }
