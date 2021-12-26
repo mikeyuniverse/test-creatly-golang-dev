2525730887034ca3ffd21ba436059badcdb312bc
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-26 11:26:51 +0300

    services.go: Update all methods

--- internal/services/services.go
+++ internal/services/services.go
@@ -1,25 +1,59 @@
 package services
 
 import (
+	"creatly-task/internal/models"
 	"creatly-task/internal/repo"
-
-	"github.com/gin-gonic/gin"
 )
 
+// TODO Create deps for uploader file in some storage "cloud"
 type Services struct {
 	db *repo.Repo
+	// cloud
 }
 
 func New(repo *repo.Repo) *Services {
 	return &Services{db: repo}
 }
 
-func (s *Services) AuthMiddleware(c *gin.Context) {}
+func (s *Services) SignUp(user *models.UserSignUpInput) error {
+	return s.db.Users.CreateUser(user)
+}
 
-func (s *Services) SignUp(c *gin.Context) {}
+// TODO Implementation SignIn
+func (s *Services) SignIn(user *models.UserSignInInput) (string, error) {
+	// Get Userdata by email from database
+	// If User not found in DB
+	//     return errors.New("email not found")
+	// If UserPasswordHash != passwordHashDB
+	//     return errors.New("password does not match")
 
-func (s *Services) SignIn(c *gin.Context) {}
+	// Generate jwt and return tokens
+	return "", nil
+}
 
-func (s *Services) Files(c *gin.Context) {}
+func (s *Services) Files() ([]models.FileOut, error) {
+	files, err := s.db.Files.All()
+	if err != nil {
+		return []models.FileOut{}, err
+	}
+	return files, nil
+}
 
-func (s *Services) UploadFile(c *gin.Context) {}
+// TODO Implementation UploadFile
+func (s *Services) UploadFile(file *models.FileUploadInput) error {
+	// Upload file to cloud server
+	// err := s.db.Files.AddLogUploadedFile(file)  // Create log for file
+	// if err != nil {
+	// 	// TODO Что делать с ошибкой? Логгировать или возвращать пользователю?
+	// 	return err
+	// }
+	return nil
+}
+
+func (s *Services) GetUserIdByToken(token string) (int64, error) {
+	userId, err := s.db.Tokens.GetUserIdByToken(token)
+	if err != nil {
+		return 0, err
+	}
+	return userId, nil
+}
