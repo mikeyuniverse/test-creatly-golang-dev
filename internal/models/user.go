7d6dc25d1ce05394017d093f6c017567875d0a76
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-27 22:05:10 +0300

    repo-users.go: Create UserSignInOutput struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,5 +1,7 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type UserSignUpInput struct {
 	Email    string `json:"email",bson:"email"`
 	Password string `json:"password",bson:"password"`
@@ -8,4 +10,11 @@ type UserSignUpInput struct {
 type UserSignInInput struct {
 	Email        string `json:"email"`
 	PasswordHash string `json:"password"`
+}
+
+type UserSignInOutput struct {
+	UserID primitive.ObjectID `bson:"_id"`
+	// UserID   string             `json:"id",bson:"_id"`
+	Email    string `json:"email",bson:"email"`
+	Password string `json:"password",bson:"password"`
 }
