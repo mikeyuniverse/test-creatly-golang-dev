3227fe870d470ca41df212ac689d8dbb9c24e1fa
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-27 22:28:47 +0300

    handlers.go: Create getTokenFromHeader, Change Files func for tests, save progress in creating AuthMiddleware

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -3,26 +3,36 @@ package server
 import (
 	"creatly-task/internal/models"
 	"creatly-task/internal/services"
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 const COOKIE_NAME = "token"
+const HEADER_WITH_TOKEN = "Authorization"
+const SIGNING_KEY = "aldnupp12nd2no"
 
 type Hasher interface {
 	Hash(password string) (string, error)
 }
 
 type Handlers struct {
-	services     *services.Services
-	MaxSizeLimit int64
-	hasher       Hasher
+	services        *services.Services
+	MaxSizeLimit    int64
+	hasher          Hasher
+	tokenHeaderName string
 }
 
-func NewHandlers(services *services.Services, FileSizeLimit int64, hasher Hasher) *Handlers {
-	return &Handlers{services: services, MaxSizeLimit: FileSizeLimit, hasher: hasher}
+func NewHandlers(services *services.Services, FileSizeLimit int64, hasher Hasher, tokenHeaderName string) *Handlers {
+	return &Handlers{
+		services:        services,
+		MaxSizeLimit:    FileSizeLimit,
+		hasher:          hasher,
+		tokenHeaderName: tokenHeaderName,
+	}
 }
 
 func (h *Handlers) SignUp(c *gin.Context) {
@@ -64,49 +74,58 @@ func (h *Handlers) SignIn(c *gin.Context) {
 
 	user.PasswordHash, err = h.hasher.Hash(user.PasswordHash)
 	if err != nil {
-		fmt.Println("ERROR: ", err.Error())
 		c.JSON(http.StatusInternalServerError, textToMap("error while encrypting password"))
 		return
 	}
 
 	token, err := h.services.SignIn(&user)
 	if err != nil {
-		fmt.Println("ERROR: ", err.Error())
 		c.JSON(http.StatusBadRequest, textToMap("invalid creds"))
 		return
 	}
 
-	c.JSON(http.StatusOK, map[string]string{"token": token})
+	c.Header("Authorization", fmt.Sprintf("Bearer %s", token))
+	c.JSON(http.StatusOK, map[string]string{"token": token}) // Additional return token in JSON response
 }
 
 func (h *Handlers) AuthMiddleware(c *gin.Context) {
-	token, err := c.Cookie(COOKIE_NAME)
+
+	token, err := h.getTokenFromHeader(c)
 	if err != nil {
-		c.JSON(http.StatusUnauthorized, textToMap("unauthorized"))
-		return
+		c.AbortWithStatus(http.StatusUnauthorized)
 	}
 
-	// TODO Check: token is valid?
+	h.services.ParseToken(token)
 
-	userId, err := h.services.GetUserIdByToken(token)
-	if err != nil {
-		c.JSON(http.StatusUnauthorized, textToMap("unauthorized"))
-		return
-	}
+	// err := h.services.ParseToken()
 
-	c.Set("userID", userId) // Pass userId in context
+	// token, err := h.checkTokenFromHeader(c)
+	// if err != nil {
+	// 	// TODO Change Abort type (delete error text)
+	// 	c.AbortWithError(http.StatusUnauthorized, err)
+	// 	return
+	// }
+
+	// 	userId, err := h.services.GetUserIdByToken(token)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusUnauthorized, textToMap("unauthorized"))
+	// 		return
+	// 	}
+
+	// 	c.Set("userID", userId) // Pass userId in context
 
 	c.Next()
 }
 
 func (h *Handlers) Files(c *gin.Context) {
-	files, err := h.services.Files()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, textToMap("error getting file data"))
-		return
-	}
+	// files, err := h.services.Files()
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, textToMap("error getting file data"))
+	// 	return
+	// }
 
-	c.JSON(http.StatusOK, files)
+	// c.JSON(http.StatusOK, files)
+	c.JSON(http.StatusOK, textToMap("access to closed functionality"))
 }
 
 func (h *Handlers) UploadFile(c *gin.Context) {
@@ -149,4 +168,26 @@ func (h *Handlers) UploadFile(c *gin.Context) {
 
 func textToMap(text string) map[string]string {
 	return map[string]string{"message": text}
+}
+
+func (h *Handlers) getTokenFromHeader(c *gin.Context) (string, error) {
+	header := c.GetHeader(h.tokenHeaderName)
+	if header == "" {
+		return "", errors.New("empty auth header")
+	}
+
+	headerParts := strings.Split(header, " ")
+	if len(headerParts) != 2 {
+		return "", errors.New("invalid auth header value")
+	}
+
+	if headerParts[0] != "Bearer" {
+		return "", errors.New("invalid auth header subject")
+	}
+
+	if len(headerParts[1]) == 0 {
+		return "", errors.New("invalid auth header token")
+	}
+
+	return headerParts[1], nil
 }
