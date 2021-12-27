84bc78b4f7508e23f39eb3bb4ceee0527a5823b3
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-27 19:21:59 +0300

    handlers.go: Add some debug print

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -30,18 +30,21 @@ func (h *Handlers) SignUp(c *gin.Context) {
 
 	err := c.BindJSON(&input)
 	if err != nil {
+		fmt.Printf("\n\n%+v\n", input)
 		c.JSON(http.StatusBadRequest, textToMap("invalid input"))
 		return
 	}
 
 	input.Password, err = h.hasher.Hash(input.Password)
 	if err != nil {
+		fmt.Println("ERROR: ", err.Error())
 		c.JSON(http.StatusInternalServerError, textToMap("error while encrypting password"))
 		return
 	}
 
 	err = h.services.SignUp(&input)
 	if err != nil {
+		fmt.Println("ERROR: ", err.Error())
 		c.JSON(http.StatusInternalServerError, textToMap("error while creating an account"))
 		return
 	}
@@ -54,18 +57,21 @@ func (h *Handlers) SignIn(c *gin.Context) {
 
 	err := c.Bind(&user)
 	if err != nil {
+		fmt.Printf("\n\n%+v\n", user)
 		c.JSON(http.StatusBadRequest, textToMap("invalid credentials"))
 		return
 	}
 
 	user.PasswordHash, err = h.hasher.Hash(user.PasswordHash)
 	if err != nil {
+		fmt.Println("ERROR: ", err.Error())
 		c.JSON(http.StatusInternalServerError, textToMap("error while encrypting password"))
 		return
 	}
 
 	token, err := h.services.SignIn(&user)
 	if err != nil {
+		fmt.Println("ERROR: ", err.Error())
 		c.JSON(http.StatusBadRequest, textToMap("invalid creds"))
 		return
 	}
