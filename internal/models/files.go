f6d2d256ad18b25fbaedee6376209b38b19c6c06
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-28 14:16:03 +0300

    models.files.go: Rename fields of FileOut struct, add bson tags

--- internal/models/files.go
+++ internal/models/files.go
@@ -1,26 +1,24 @@
 package models
 
-import "mime/multipart"
-
 type FileOut struct {
-	Filename       string `json:"filename"`
-	Size           int    `json:"size"`
-	UploadDate     int64  `json:"uploadDate"`
-	UploadedUserId int    `json:"userId"`
-	FileURL        string `json:"fileUrl"`
+	Filename string `json:"filename",bson:"filename"`
+	Size     int    `json:"size",bson:"size"`
+	Date     int64  `json:"uploadDate",bson:"date"`
+	UserId   string `json:"userId",bson:"userId"`
+	Url      string `json:"url",bson:"url"`
 }
 
 type FileUploadInput struct {
 	Filename string `json:"filename"`
 	Size     int64  `json:"size"`
-	UserId   int    `json:"userId"`
-	FileData multipart.File
+	UserId   string `json:"userId"`
+	FileData []byte
 }
 
 type FileUploadLogInput struct {
-	Size        int64
-	UploadDate  int64
-	Filename    string
-	UserId      int
-	StorageLink string
+	Size       int64  `bson:"size"`
+	UploadDate int64  `bson:"date"`
+	Filename   string `bson:"filename"`
+	UserId     string `bson:"userId"`
+	Url        string `bson:"url"`
 }
