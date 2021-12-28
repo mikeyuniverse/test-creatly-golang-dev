bd57825ddc2214b627e81b0821e0b8fe154917a3
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-28 14:16:58 +0300

    repo.go: Add AddLog method to Files interface, change repo constructor

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -4,7 +4,6 @@ import (
 	"creatly-task/internal/config"
 	"creatly-task/internal/models"
 	"creatly-task/internal/mongodb"
-	"creatly-task/pkg/storage"
 )
 
 type Users interface {
@@ -18,6 +17,7 @@ type Tokens interface {
 
 type Files interface {
 	All() ([]models.FileOut, error)
+	AddLog(log *models.FileUploadLogInput) error
 }
 
 type Repo struct {
@@ -27,7 +27,7 @@ type Repo struct {
 }
 
 // TODO Implementation of interfaces
-func New(db *mongodb.Mongo, storage *storage.Storage, config *config.Repo) *Repo {
+func New(db *mongodb.Mongo, config *config.Repo) *Repo {
 	return &Repo{
 		Users:  newUsersRepo(db, config.UsersCollection),
 		Tokens: newTokensRepo(db, config.TokensCollection),
