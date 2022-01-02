ec0142d9ef340801c1ff746bdb44397d8c0b5c38
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2022-01-03 02:42:30 +0300

    handlers_test.go: Create tests for SignIn

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -102,7 +102,92 @@ func Test_SignUp(t *testing.T) {
 	}
 }
 
-func Test_SignIn(t *testing.T) {}
+func Test_SignIn(t *testing.T) {
+	testTable := []struct {
+		name           string
+		behavior       func(mh *mock_handlers.MockHasher, s *mock_handlers.MockServices)
+		bodyInput      string
+		outStatusCode  int
+		outMessage     string
+		outHeaderValue string
+	}{
+		{
+			name:          "OK",
+			bodyInput:     `{"email": "some@mail.com", "password": "qwerty"}`,
+			outStatusCode: 200,
+			outMessage:    `{"token":"token"}`,
+			behavior: func(mh *mock_handlers.MockHasher, s *mock_handlers.MockServices) {
+				mh.EXPECT().Hash("qwerty").Return("ytrewq", nil)
+				s.EXPECT().SignIn(&models.UserSignInInput{
+					Email:        "some@mail.com",
+					PasswordHash: "ytrewq",
+				}).Return("token", nil)
+			},
+			outHeaderValue: "Bearer token",
+		},
+		{
+			name:           "ERROR: invalid creds",
+			bodyInput:      `{"email": "", "password": "qwerty"}`,
+			outStatusCode:  400,
+			outMessage:     `{"message":"invalid credentials"}`,
+			behavior:       func(mh *mock_handlers.MockHasher, s *mock_handlers.MockServices) {},
+			outHeaderValue: "",
+		},
+		{
+			name:          "ERROR: hash password error",
+			bodyInput:     `{"email": "some@mail.com", "password": "qwerty"}`,
+			outStatusCode: 500,
+			outMessage:    `{"message":"error while encrypting password"}`,
+			behavior: func(mh *mock_handlers.MockHasher, s *mock_handlers.MockServices) {
+				mh.EXPECT().Hash("qwerty").Return("", errors.New("internal error"))
+			},
+			outHeaderValue: "",
+		},
+		{
+			name:          "ERROR: sign-in service error",
+			bodyInput:     `{"email": "some@mail.com", "password": "qwerty"}`,
+			outStatusCode: 400,
+			outMessage:    `{"message":"invalid creds"}`,
+			behavior: func(mh *mock_handlers.MockHasher, s *mock_handlers.MockServices) {
+				mh.EXPECT().Hash("qwerty").Return("ytrewq", nil)
+				s.EXPECT().SignIn(&models.UserSignInInput{
+					Email:        "some@mail.com",
+					PasswordHash: "ytrewq",
+				}).Return("", errors.New("internal error"))
+			},
+			outHeaderValue: "",
+		},
+	}
+
+	for _, test := range testTable {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			hasher := mock_handlers.NewMockHasher(ctrl)
+			services := mock_handlers.NewMockServices(ctrl)
+
+			test.behavior(hasher, services)
+
+			handlers := New(services, 100000, hasher, "Authorization", "userId")
+
+			r := gin.New()
+			r.POST("/sign-in", handlers.SignIn)
+
+			// Create Request
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/sign-in", bytes.NewBufferString(test.bodyInput))
+
+			// Make Request
+			r.ServeHTTP(w, req)
+
+			// Assert
+			assert.Equal(t, test.outStatusCode, w.Code)
+			assert.Equal(t, test.outMessage, w.Body.String())
+			assert.Equal(t, w.Header().Get("Authorization"), test.outHeaderValue)
+		})
+	}
+}
 
 func Test_AuthMiddleware(t *testing.T) {}
 
