d8060e481be20aa34e26770bcb48e69f1fd1387f
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2022-01-03 00:55:17 +0300

    handlers_test.go: Create template for handlers tests

---
+++ internal/server/handlers_test.go
@@ -0,0 +1,19 @@
+package server
+
+import "testing"
+
+func Test_SignUp(t *testing.T) {}
+
+func Test_SignIn(t *testing.T) {}
+
+func Test_AuthMiddleware(t *testing.T) {}
+
+func Test_Files(t *testing.T) {}
+
+func Test_UploadFile(t *testing.T) {}
+
+func Test_getTokenFromHeader(t *testing.T) {}
+
+func Test_textToMap(t *testing.T) {}
+
+func Test_readBody(t *testing.T) {}
